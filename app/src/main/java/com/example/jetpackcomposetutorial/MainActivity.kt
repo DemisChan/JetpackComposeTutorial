13d0a64fd35f31ffe85aff0640cdbafa4c146350
Demis Lavrentidis <demilavr@gmail.com>
2023-01-31 17:41:34 +0000

    -added Composables for image and text -added modifiers to text and image as per task

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,19 +27,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+ArticleTutorialWithImage(title = getString(R.string.app_name), description = getString(R.string.description_text), tutorial = getString(R.string.requirement_text))
                 }
             }
         }
     }
 }
 
+
 @Composable
+fun ArticleTutorialWithText(title: String, description: String, tutorial: String) {
+    Column {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = tutorial,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+
+    }
+}
+
+@Composable
+fun ArticleTutorialWithImage(title: String, description: String, tutorial: String) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+
+        )
+        ArticleTutorialWithText(title = title, description = description, tutorial = tutorial)
+    }
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTutorialTheme {
-
+        ArticleTutorialWithImage(
+            title = "New App",
+            description = "This app does....",
+            tutorial = "Create a project"
+        )
     }
 }
